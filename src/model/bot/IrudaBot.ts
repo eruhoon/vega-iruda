e785b18adf5060507cd8c2edac04d9a308a6ab27
z1z2z3z4 <46558914+z1z2z3z4@users.noreply.github.com>
2021-05-25 18:21:50 +0900

    add SearchRule
    !검색 명령어

--- src/model/bot/IrudaBot.ts
+++ src/model/bot/IrudaBot.ts
@@ -1,6 +1,7 @@
 import { Bot } from '../../framework/bot/Bot';
 import { HelpRule } from '../rule/HelpRule';
 import { NamuWikiRule } from '../rule/NamuWikiRule';
+import { SearchRule } from '../rule/SearchRule';
 
 export class IrudaBot extends Bot {
   public constructor() {
