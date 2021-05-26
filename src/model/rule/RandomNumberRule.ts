7ad65058f115dfc89c24886b3918793b55f31cec
엘풍 <eruhoon@gmail.com>
2021-05-26 23:58:44 +0900

    format RandomNumberRule

--- src/model/rule/RandomNumberRule.ts
+++ src/model/rule/RandomNumberRule.ts
@@ -7,9 +7,9 @@ export class RandomNumberRule implements TextRule {
 
   public async makeMessage(src: string): Promise<string> {
     const match = /!랜덤 (.*)/.exec(src);
-    let number = (match )? parseInt(match[1]) : NaN;
-    if (!isNaN(number) && (number>31415 || number<2)) number = NaN;
-    const word = (!isNaN(number))? Math.floor(Math.random()*number)+1: -1;
+    let number = match ? parseInt(match[1]) : NaN;
+    if (!isNaN(number) && (number > 31415 || number < 2)) number = NaN;
+    const word = !isNaN(number) ? Math.floor(Math.random() * number) + 1 : -1;
     return `${word}`;
   }
 }
