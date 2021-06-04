c2ab34aeddd091fda9ea998b1b3ac2700b6e9a53
엘풍 <eruhoon@gmail.com>
2021-06-04 19:07:42 +0900

    refactor MeguminRule

--- src/model/rule/MeguminRule.ts
+++ src/model/rule/MeguminRule.ts
@@ -8,31 +8,43 @@ export class MeguminRule extends TargetDateRuleTemplate {
   }
 
   public async makeMessage(src: string): Promise<Response> {
-    const curDate = new Date();
-    const target = this.createTargetTime({ hour: 18 });
-    const endDate = this.createTargetDate(2022, 3, 18);
-    const endDiff = this.getDiff(endDate.getTime());
+    const resultMsg = this.getDiffMessage();
+    const duedateMsg = this.getDuedateMessage();
+    const msg = [resultMsg, duedateMsg].join(' ');
+    return new TextResponse(msg);
+  }
 
+  private getDiffMessage(): string {
+    const { diff, late } = this.getStatus();
+    const workMsg = `탈출 시간까지 ${this.getDiffString(diff)}`;
+    const outWorkMsg = '이미 놀고있는 메구밍입니다.';
+    return late ? outWorkMsg : workMsg;
+  }
+
+  private getStatus(): Status {
+    const now = new Date();
+    const target = this.createTargetTime({ hour: 18 });
     const todayDiff = this.getDiff(target.getTime());
     const oneDay = 24 * 60 * 60 * 1000;
     const tomorrowDiff = this.getDiff(target.getTime() + oneDay);
-
-    const alreadyLate = todayDiff.hour < 0 || curDate.getHours() < 7;
-    const diff = alreadyLate ? tomorrowDiff : todayDiff;
-
-    const prefix = '탈출 시간까지 ';
-    const timeMsg = this.getDiffString(diff);
-    const baseMsg = prefix + timeMsg;
-    const outWorkMsg = '이미 놀고있는 메구밍입니다.';
-    const resultMsg = alreadyLate ? outWorkMsg : baseMsg;
-    const dayMsg = ` 탈출까지 ${endDiff.day + 1}일 남았습니다.`;
-    const msg = resultMsg + dayMsg;
-
-    return new TextResponse(msg);
+    const late = todayDiff.hour < 0 || now.getHours() < 7;
+    const diff = late ? tomorrowDiff : todayDiff;
+    return { diff, late };
   }
 
   private getDiffString(diff: DiffTime): string {
     const { hour, minute, second } = diff;
     return `${hour}시 ${minute}분 ${second}초!`;
   }
+
+  private getDuedateMessage(): string {
+    const target = this.createTargetDate(2022, 3, 18);
+    const diff = this.getDiff(target.getTime());
+    return ` 탈출까지 ${diff.day + 1}일 남았습니다.`;
+  }
 }
+
+type Status = {
+  diff: DiffTime;
+  late: boolean;
+};
