325ca07b8ab7a09cae091f3ef1c8029e6986da9c
엘풍 <eruhoon@gmail.com>
2021-06-04 18:55:01 +0900

    refactor MeguminRule

--- src/model/rule/MeguminRule.ts
+++ src/model/rule/MeguminRule.ts
@@ -1,51 +1,38 @@
 import { Response } from '../../framework/response/Response';
 import { TextResponse } from '../../framework/response/TextReponse';
-import { TextRule } from '../../framework/rule/TextRule';
+import { DiffTime, TargetDateRuleTemplate } from './TargetDateRuleTemplate';
 
-export class MeguminRule extends TextRule {
+export class MeguminRule extends TargetDateRuleTemplate {
   public match(src: string): boolean {
     return src == '!메' || src == '!메구밍';
   }
 
   public async makeMessage(src: string): Promise<Response> {
     const curDate = new Date();
-    const targetDate = new Date(
-      curDate.getFullYear(),
-      curDate.getMonth(),
-      curDate.getDate(),
-      18,
-      0,
-      0
-    );
-    const currentDate = new Date();
-    const endDate = new Date("2022-03-18");
-    const diffDays = Math.floor((endDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24));
+    const target = this.createTargetTime({ hour: 18 });
+    const endDate = this.createTargetDate(2022, 3, 18);
+    const endDiff = this.getDiff(endDate.getTime());
 
-    let diffDate = +targetDate - +curDate;
-    let alreadyLate = diffDate < 0 || curDate.getHours() < 7;
+    const todayDiff = this.getDiff(target.getTime());
+    const oneDay = 24 * 60 * 60 * 1000;
+    const tomorrowDiff = this.getDiff(target.getTime() + oneDay);
 
-    // add 1day to prevent minus time
-    diffDate = alreadyLate ? diffDate + 3600 * 24 : diffDate;
-    diffDate /= 1000;
+    const alreadyLate = todayDiff.hour < 0 || curDate.getHours() < 7;
+    const diff = alreadyLate ? tomorrowDiff : todayDiff;
 
-    const diffHour = diffDate / 3600;
-    const diffMin = (diffDate % 3600) / 60;
-    const diffSec = diffDate % 60;
-    let msg =
-      '탈출 시간까지 ' +
-      diffHour.toFixed() +
-      '시 ' +
-      diffMin.toFixed() +
-      '분 ' +
-      diffSec.toFixed() +
-      '초!';
-
-    if (alreadyLate) {
-      msg = '이미 놀고있는 메구밍입니다.';
-    }
-    
-    msg += ` 탈출까지 ${diffDays}일 남았습니다.`
+    const prefix = '탈출 시간까지 ';
+    const timeMsg = this.getDiffString(diff);
+    const baseMsg = prefix + timeMsg;
+    const outWorkMsg = '이미 놀고있는 메구밍입니다.';
+    const resultMsg = alreadyLate ? outWorkMsg : baseMsg;
+    const dayMsg = ` 탈출까지 ${endDiff.day + 1}일 남았습니다.`;
+    const msg = resultMsg + dayMsg;
 
     return new TextResponse(msg);
+  }
+
+  private getDiffString(diff: DiffTime): string {
+    const { hour, minute, second } = diff;
+    return `${hour}시 ${minute}분 ${second}초!`;
   }
 }
