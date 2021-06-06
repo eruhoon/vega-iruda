95cbf58cc1d6400428c3fc836831113c8d77761c
엘풍 <eruhoon@gmail.com>
2021-06-07 08:48:58 +0900

    extract WeatherLoader

--- src/model/rule/WeatherRule.ts
+++ src/model/rule/WeatherRule.ts
@@ -1,10 +1,8 @@
-import axios from 'axios';
-import Cheerio from 'cheerio';
-import * as iconv from 'iconv-lite';
 import { GeneralPurposeCardBody } from '../../framework/response/body/GeneralPurposeCardBody';
 import { GeneralPurposeCardResponse } from '../../framework/response/GeneralPurposeCardResponse';
 import { Response } from '../../framework/response/Response';
 import { TextResponse } from '../../framework/response/TextReponse';
+import { WeatherLoader } from '../loader/weather/WeatherLoader';
 import { ArgumentRuleTemplate } from './ArgumentRuleTemplate';
 
 export class WeatherRule extends ArgumentRuleTemplate {
@@ -12,9 +10,11 @@ export class WeatherRule extends ArgumentRuleTemplate {
     'https://www.weather.go.kr/weather/main-now-weather.jsp';
   private weathersCache: Weather[] | null = null;
   private expires: number = 0;
+  private loader: WeatherLoader;
 
   public constructor() {
     super('날씨');
+    this.loader = new WeatherLoader();
   }
 
   protected async makeMessageWithArg(arg: string): Promise<Response> {
@@ -37,37 +37,10 @@ export class WeatherRule extends ArgumentRuleTemplate {
   private async getWeathers(): Promise<Weather[]> {
     const timestamp = new Date().getTime();
     if (this.weathersCache === null || timestamp >= this.expires) {
-      this.weathersCache = await this.getWeatherFromWeb();
+      this.weathersCache = await this.loader.load();
       this.expires = timestamp + 60 * 60 * 1000;
     }
     return this.weathersCache;
-  }
-
-  private async getWeatherFromWeb(): Promise<Weather[]> {
-    const { data: rawBody } = await axios.get(this.URL, {
-      responseType: 'arraybuffer',
-    });
-    const data = iconv.decode(Buffer.from(rawBody), 'EUC-KR').toString();
-
-    if (!data) {
-      return [];
-    }
-    const $ = Cheerio.load(data, {
-      normalizeWhitespace: true,
-    });
-    const $weathers = $('#weather dl');
-    const weathers: Weather[] = $weathers.toArray().map((e) => {
-      const $e = $(e);
-      const name = $e.find('dt').eq(0).text();
-      const weatherAlt = $e.find('dd.weather').attr('alt');
-      const weather = weatherAlt ? weatherAlt.toString() : '';
-      const imgSrc = $e.find('img').attr('src');
-      const imgPath = imgSrc ? imgSrc.toString() : '';
-      const img = `https://www.weather.go.kr${imgPath}`;
-      const temp = parseFloat($e.find('dd.temp').text());
-      return { name, weather, temp, img };
-    });
-    return weathers;
   }
 }
 
