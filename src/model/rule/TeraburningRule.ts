c2b6f61a2f974d55eef6ec306b897bc9c0e588e6
Maackia <maackia.flos@gmail.com>
2021-06-17 19:05:08 +0900

    fix to comfortable

--- src/model/rule/TeraburningRule.ts
+++ src/model/rule/TeraburningRule.ts
@@ -2,7 +2,7 @@ import { Response } from '../../framework/response/Response';
 import { TextResponse } from '../../framework/response/TextReponse';
 import { TextRule } from '../../framework/rule/TextRule';
 
-export class TeraburningRule extends TextRule {
+export class TeraBurningRule extends TextRule {
   public match(src: string): boolean {
     return src == '!테라버닝';
   }
