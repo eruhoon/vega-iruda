d7d8f8fa696f48bd6d44e74e6456d6cf5b285a62
엘풍 <eruhoon@gmail.com>
2021-07-11 18:39:30 +0900

    update LolScheduleRule

---
+++ src/model/loader/lol/schedule/LolScheduleLoader.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export class LolScheduleLoader {
+  public async load(): Promise<Event[]> {
+    const url =
+      'https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=ko-KR&leagueId=98767991310872058%2C98767991335774713';
+    const { data } = await axios.get(url, {
+      headers: { 'x-api-key': '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z' },
+    });
+    const events: Event[] = data.data.schedule.events || [];
+    return events;
+  }
+}
+
+type Event = {
+  startTime: string;
+  state: 'unstarted' | 'in progress' | 'completed';
+  league: League;
+  match: Match;
+};
+
+type League = {
+  name: string;
+  slug: string;
+};
+
+type Match = {
+  id: string;
+  teams: Team[];
+};
+
+type Team = {
+  name: string;
+  code: string;
+  image: string;
+};
--- src/model/rule/LolScheduleRule.ts
+++ src/model/rule/LolScheduleRule.ts
@@ -2,6 +2,7 @@ import { GeneralPurposeCarouselResponse } from '../../framework/response/General
 import { Response } from '../../framework/response/Response';
 import { TextResponse } from '../../framework/response/TextReponse';
 import { TextRule } from '../../framework/rule/TextRule';
+import { LolScheduleLoader } from '../loader/lol/schedule/LolScheduleLoader';
 
 export class LolScheduleRule extends TextRule {
   public match(src: string): boolean {
@@ -14,15 +15,14 @@ export class LolScheduleRule extends TextRule {
     );
   }
 
-  public async makeMessage(src: string): Promise<Response> {
+  private getTodayTimeStamp(): number {
     const date = new Date();
     const ONE_DAY = 24 * 60 * 60 * 1000;
-    const today = date.getTime() - (date.getTime() % ONE_DAY);
-    const found = this.schedules.find((s) => {
-      const timestamp = new Date(s.date).getTime();
-      return today === timestamp;
-    });
+    return date.getTime() - (date.getTime() % ONE_DAY);
+  }
 
+  public async makeMessage(src: string): Promise<Response> {
+    const found = await this.load();
     if (!found) {
       return new TextResponse(
         'https://lolesports.com/schedule?leagues=lck,worlds'
@@ -43,171 +43,27 @@ export class LolScheduleRule extends TextRule {
     );
   }
 
-  private leagues: string[] = [
-    'https://am-a.akamaihd.net/image?resize=60:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2Flck-color-on-black.png',
-    'https://am-a.akamaihd.net/image?resize=60:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2Flck-cl-white.png',
-  ];
+  private async load(): Promise<Schedule> {
+    const loaded = await new LolScheduleLoader().load();
+    const today = this.formatDate(new Date());
+    const schedules: [string, string][] = loaded
+      .filter((e) => {
+        return today === this.formatDate(new Date(e.startTime));
+      })
+      .map((e) => {
+        const teams = e.match.teams.map((t) => t.code);
+        return [teams[0], teams[1]];
+      });
+    return { date: today, schedules };
+  }
 
-  private schedules: Schedule[] = [
-    {
-      date: '2021-06-19',
-      schedules: [
-        ['DK', 'DRX'],
-        ['KT', 'AF'],
-      ],
-    },
-    {
-      date: '2021-06-20',
-      schedules: [
-        ['LSB', 'T1'],
-        ['BRO', 'HLE'],
-      ],
-    },
-
-    {
-      date: '2021-06-23',
-      schedules: [
-        ['HLE', 'AF'],
-        ['DK', 'BRO'],
-      ],
-    },
-    {
-      date: '2021-06-24',
-      schedules: [
-        ['GEN', 'LSB'],
-        ['T1', 'NS'],
-      ],
-    },
-    {
-      date: '2021-06-25',
-      schedules: [
-        ['KT', 'DRX'],
-        ['DK', 'HLE'],
-      ],
-    },
-    {
-      date: '2021-06-26',
-      schedules: [
-        ['AF', 'GEN'],
-        ['NS', 'BRO'],
-      ],
-    },
-    {
-      date: '2021-06-27',
-      schedules: [
-        ['DRX', 'T1'],
-        ['KT', 'LSB'],
-      ],
-    },
-    {
-      date: '2021-06-30',
-      schedules: [
-        ['T1', 'KT'],
-        ['BRO', 'GEN'],
-      ],
-    },
-
-    {
-      date: '2021-07-01',
-      schedules: [
-        ['HLE', 'DRX'],
-        ['AF', 'LSB'],
-      ],
-    },
-    {
-      date: '2021-07-02',
-      schedules: [
-        ['NS', 'DK'],
-        ['BRO', 'KT'],
-      ],
-    },
-    {
-      date: '2021-07-03',
-      schedules: [
-        ['T1', 'AF'],
-        ['LSB', 'DRX'],
-      ],
-    },
-    {
-      date: '2021-07-04',
-      schedules: [
-        ['HLE', 'NS'],
-        ['DK', 'GEN'],
-      ],
-    },
-
-    {
-      date: '2021-07-07',
-      schedules: [
-        ['LSB', 'DK'],
-        ['AF', 'NS'],
-      ],
-    },
-    {
-      date: '2021-07-08',
-      schedules: [
-        ['KT', 'HLE'],
-        ['DRX', 'BRO'],
-      ],
-    },
-    {
-      date: '2021-07-09',
-      schedules: [
-        ['GEN', 'T1'],
-        ['LSB', 'AF'],
-      ],
-    },
-    {
-      date: '2021-07-10',
-      schedules: [
-        ['KT', 'BRO'],
-        ['HLE', 'DK'],
-      ],
-    },
-  ];
-
-  private teams: LolTeam[] = [
-    {
-      name: 'KT',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FNSFullonDark.png',
-    },
-    {
-      name: 'T1',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592589079394_T1T1-01-FullonDark.png',
-    },
-    {
-      name: 'LSB',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2Fliiv-sandbox-new.png',
-    },
-    {
-      name: 'HLE',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2Fhle-2021-color-on-dark.png',
-    },
-    {
-      name: 'BRO',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FBRO_color_darkbackground.png',
-    },
-    {
-      name: 'DK',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FDK-FullonDark.png',
-    },
-    {
-      name: 'DRX',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592589284897_DRXDRX-01-FullonDark.png',
-    },
-    {
-      name: 'GEN',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592589327622_Gen.GGEN-01-FullonDark.png',
-    },
-    {
-      name: 'NS',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FNSFullonDark.png',
-    },
-    {
-      name: 'AF',
-      icon: 'https://am-a.akamaihd.net/image?resize=70:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FAF-FullonDark.png',
-    },
-  ];
+  private formatDate(dateObj: Date): string {
+    const format = (src: number) => (src < 10 ? `0${src}` : `${src}`);
+    const year = dateObj.getFullYear();
+    const month = dateObj.getMonth() + 1;
+    const date = dateObj.getDate();
+    return `${year}-${format(month)}-${format(date)}`;
+  }
 }
 
 type LolTeam = {
