d65c4ab143f9e4d34bf5fbdeb5cae43da65a6b45
Whimsicott <eruhoon@gmail.com>
2021-07-18 20:40:32 +0900

    add Bot#escapeRegex

--- src/framework/bot/Bot.ts
+++ src/framework/bot/Bot.ts
@@ -8,7 +8,12 @@ export class Bot {
   }
 
   public async process(src: string): Promise<string | null> {
-    const matched = this.#rules.find((rule) => rule.match(src));
+    const escaped = this.#escapeRegex(src);
+    const matched = this.#rules.find((rule) => rule.match(escaped));
     return matched ? (await matched.makeMessage(src)).serialize() : null;
+  }
+
+  #escapeRegex(src: string) {
+    return src.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
   }
 }
