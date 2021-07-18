8aaabd9fb79258356a12347e13a1bc52f152ce08
Whimsicott <eruhoon@gmail.com>
2021-07-18 22:17:11 +0900

    optimize import

--- src/framework/response/TextReponse.ts
+++ src/framework/response/TextReponse.ts
@@ -1,6 +1,5 @@
 import { TextBody } from './body/TextBody';
 import { Response } from './Response';
-import { ResponseType } from './ResponseType';
 
 export class TextResponse extends Response {
   constructor(text: string) {
