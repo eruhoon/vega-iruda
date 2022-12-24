86a6eb25808f1fe699e10c8e6e4f4c9666bdeb75
peng_jo <85087860+peng-jo@users.noreply.github.com>
2022-12-25 08:29:20 +0900

    Generate CityWeatherRule
    !웨더

---
+++ src/model/rule/CityWeatherRule.ts
@@ -0,0 +1,41 @@
+import { GeneralPurposeCardResponse } from '../../framework/response/GeneralPurposeCardResponse';
+import { Response } from '../../framework/response/Response';
+import { TextResponse } from '../../framework/response/TextReponse';
+import { ExpiredCacheLoader } from '../loader/ExpiredCacheLoader';
+import { Loader } from '../loader/Loader';
+import { CityWeatherLoader } from '../loader/weather/CityWeatherLoader';
+import { ArgumentRuleTemplate } from './ArgumentRuleTemplate';
+
+export class CityWeatherRule extends ArgumentRuleTemplate {
+  readonly #URL = 'https://www.weather.go.kr/';
+  #loader: Loader<Weather>;
+
+  constructor() {
+    super('웨더');
+    this.#loader = new CityWeatherLoader();
+  }
+
+  protected async makeMessageWithArg(arg: string): Promise<Response> {
+    const city = arg;
+    const weather = await this.#loader.load(city);
+    const FAILTEMP = -9999;
+    // const weather = weathers.find((w) => w.name === city);
+    if (!weather || weather.temp === FAILTEMP || weather.temp2 === FAILTEMP ) {
+      return new TextResponse('검색 실패');
+    }
+    return new GeneralPurposeCardResponse({
+      link: this.#URL,
+      title: `${city} (${weather.weather})`,
+      icon: weather.img,
+      subtitle: `${weather.temp}℃(${weather.temp2}℃)`,
+    });
+  }
+}
+
+type Weather = {
+  name?: string;
+  weather: string;
+  temp: number;
+  temp2: number;
+  img: string;
+};
