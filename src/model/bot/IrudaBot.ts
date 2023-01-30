4b84fd8f3d6627627524558c2d8445f1419750c3
엘풍 <eruhoon@gmail.com>
2023-01-31 00:28:17 +0900

    remove deprecatedRule

--- src/model/bot/IrudaBot.ts
+++ src/model/bot/IrudaBot.ts
@@ -1,20 +1,10 @@
 import { Bot } from '../../framework/bot/Bot';
-import { BojRule } from '../rule/BojRule';
 import { HelpRule } from '../rule/HelpRule';
 import { LoLSchessSearchRule } from '../rule/LoLSchessSearchRule';
 import { LolUserRule } from '../rule/LolUserRule';
-import { MapleEventRule } from '../rule/MapleEventRule';
-import { TwitchRule } from '../rule/TwitchRule';
 
 export class IrudaBot extends Bot {
   constructor() {
-    super([
-      new HelpRule(),
-      new MapleEventRule(),
-      new TwitchRule(),
-      new BojRule(),
-      new LoLSchessSearchRule(),
-      new LolUserRule(),
-    ]);
+    super([new HelpRule(), new LoLSchessSearchRule(), new LolUserRule()]);
   }
 }
